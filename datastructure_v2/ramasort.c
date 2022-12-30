858450bd99f5bf5a3877f0d3f7750f75fb438743
EP2022 11808598 <ep11808598@g0.complang.tuwien.ac.at>
2022-12-30 15:02:06 +0100

    cleanup

--- datastructure_v2/ramasort.c
+++ datastructure_v2/ramasort.c
@@ -20,21 +20,6 @@ struct entry
   long value;
 };
 
-int comp_entry(const void *p1, const void *p2)
-{
-  const struct entry *e1 = p1;
-  const struct entry *e2 = p2;
-  if (e1->value < e2->value)
-    return -1;
-  else
-    return e1->value > e2->value;
-}
-
-long cube(long n)
-{
-  return n * n * n;
-}
-
 size_t size_heap(long n)
 /* compute a table size that is large enough to keep all I^3+J^3<n */
 {
@@ -70,7 +55,7 @@ int Insert(struct entry element)
   return now;
 }
 
-int CheckForOneSameSum(int i)
+int CheckForOneSameSum()
 {
   int c = 0;
 
@@ -80,6 +65,14 @@ int CheckForOneSameSum(int i)
   {
     if (heap[2].value == val)
       c++;
+  }
+  if (3 < heapSize)
+  {
+    if (heap[3].value == val)
+      if (c == 1)
+        return 0;
+      else
+        c++;
   }
   if (4 < heapSize)
   {
@@ -92,14 +85,6 @@ int CheckForOneSameSum(int i)
   if (5 < heapSize)
   {
     if (heap[5].value == val)
-      if (c == 1)
-        return 0;
-      else
-        c++;
-  }
-  if (3 < heapSize)
-  {
-    if (heap[3].value == val)
       if (c == 1)
         return 0;
       else
@@ -139,15 +124,15 @@ struct entry DeleteMin()
    If it does not fit, take minimum element among both its children and replaces parent with it.
    Again See if the last element fits in that place.*/
   struct entry minElement, lastElement;
-  int now, child;
+  int now, child, now2;
   minElement = heap[1];
   lastElement = heap[heapSize--];
   /* now refers to the index at which we are now */
-  for (now = 1; now * 2 <= heapSize; now = child)
+  for (now = 1, now2 = 2; now2 <= heapSize; now = child, now2 = now << 1)
   {
     /* child is the index of the element which is minimum among both the children */
     /* Indexes of children are i*2 and i*2 + 1*/
-    child = now * 2;
+    child = now2;
     /*child!=heapSize beacuse heap[heapSize+1] does not exist, which means it has only one
      child */
     if (child != heapSize && heap[child + 1].value < heap[child].value)
@@ -206,16 +191,6 @@ struct entry ReplaceMin(struct entry newEntry)
   return minElement;
 }
 
-void printHeap()
-{
-
-  for (int i = 1; i <= heapSize; i++)
-  {
-    printf("k %ld, l %ld, value %ld\n", heap[i].k, heap[i].l, heap[i].value);
-  }
-
-  printf("\n");
-}
 // HEAP    ///////////////////////////////////////////////////////
 
 int main(int argc, char **argv)
@@ -231,60 +206,44 @@ int main(int argc, char **argv)
   n = strtol(argv[1], &endptr, 10);
   if (*endptr != '\0')
     goto usage;
-  // table_size = size_table(n);
-  // table = calloc(table_size,sizeof(struct entry));
+
   maxNumbers = size_heap(n);
   heap = malloc((maxNumbers + 10) * sizeof(struct entry));
 
   Init();
 
-  for (int j = 1; j <= maxNumbers; j++)
+  for (long j = 1; j <= maxNumbers; j++)
   {
-    Insert((struct entry){0, j, cube(j)});
+    Insert((struct entry){0, j, j * j * j});
   }
-  printf("hSize %ld\n", maxNumbers);
-
-  int cc = 0;
+  int iterations = 0;
   while (heapSize > 0)
   {
-    cc++;
-    // printf("size %ld\n", heapSize);
-    // printHeap();
-    if (CheckForOneSameSum(1))
+    iterations++;
+    struct entry min = heap[1];
+    if (CheckForOneSameSum())
     {
       count++;
-      // printf("found:       k %ld, l %ld, value %ld, size %ld\n", heap[1].k, heap[1].l, heap[1].value, heapSize);
-      checksum += heap[1].value;
+      checksum += min.value;
     }
-
-    struct entry min = heap[1]; //DeleteMin();
-    // printf("k%ld, l%ld, value %ld\n",min.k, min.l, min.value);
 
     // Insert new node if i < j
     min.k++;
-    if (min.k < min.l && cube(min.k) + cube(min.l) <= n)
+    long cubeSum = min.k * min.k * min.k + min.l * min.l * min.l;
+    if (min.k < min.l && cubeSum <= n)
     {
-      min.value = cube(min.k) + cube(min.l);
+      min.value = cubeSum;
       ReplaceMin(min);
-    }else{
+    }
+    else
+    {
       DeleteMin();
     }
   }
 
-  /*assert(m<=table_size);
-  qsort(table, m, sizeof(struct entry), comp_entry);
-  for (i=1; i<m; i++) {
-    if (table[i-1].value==table[i].value) {
-      count++;
-      checksum += table[i].value;
-      while (i<m-1 && table[i].value==table[i+1].value)
-        i++;
-    }
-  } */
-
+  printf("iterations %d, run time %f\n", iterations, (iterations / cbrt(n * n)));
   printf("%ld Ramanujan numbers up to %ld, checksum=%ld\noccupation=%ld, size=%ld\n", count, n, checksum, m, 0);
   printf("Memory usage: >=%ld\n", maxNumbers * sizeof(struct entry));
-  printf("cc %ld\n", cc);
 
   return 0;
 
